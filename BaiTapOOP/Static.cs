090093d6a3057cad193b8c61b8880c53a02c13ca
TamKem <tamkem@gmail.com>
2023-03-12 10:31:55 +0700

    test task

---
+++ BaiTapOOP/Static.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapOOP
+{
+     static class QuyUoc
+    {
+        private static long minGD;
+        private static long maxGD;
+        private static int CuocPhi;
+
+        static QuyUoc()
+        {
+            minGD = 10000;
+            maxGD = 1000000;
+            CuocPhi = 5000;
+        }
+
+        public static void QuyUocGD()
+        {
+            Console.WriteLine("Muc Thap Nhat Co The Giao Dich : " + minGD);
+            Console.WriteLine("Muc Cao Nhat Co The Giao Dich : " + maxGD);
+            Console.WriteLine("Cuoc Phi 1 Lan Giao Dich : " + CuocPhi);
+        }
+
+        public static long getMinGD()
+        {
+            return minGD;
+        }
+
+        public static long getMaxGD()
+        {
+            return maxGD;
+        }
+
+        public static int getCuocPhi()
+        {
+            return CuocPhi;
+        }
+    }
+}
