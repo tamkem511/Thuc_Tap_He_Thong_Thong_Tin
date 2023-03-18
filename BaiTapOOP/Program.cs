f40ec980a631c70fe98bb668b497db189308b4d3
TamKem <tamkem@gmail.com>
2023-03-18 22:17:13 +0700

    do task week 1

--- BaiTapOOP/Program.cs
+++ BaiTapOOP/Program.cs
@@ -1,54 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+/*
+ Đề bài : Create a console application in chapter 03 named Exercise 04 that ask the user for
+two numbers in the range 0-255 and then divides the first number by the second:
 
-namespace BaiTapOOP
+and write exception handlers to catch any thrown errors, as shown in the following output
+ */
+
+public class Task1
 {
-    internal class Start
+    public static void InputValue(ref int num1, ref int num2)
     {
-        static void Main(string[] args)
+        try
         {
-            ThucHienGiaoDich x = new ThucHienGiaoDich();
-            Console.WriteLine("========= Quy Uoc Giao Dich =========");
-            QuyUoc.QuyUocGD();
-            Console.WriteLine("======== Chuong Trinh ========");
-            Console.WriteLine("1.Xem So Du Tai Khoan");
-            Console.WriteLine("2.Nap Tien");
-            Console.WriteLine("3.Rut Tien");
-            Console.WriteLine("4.Giao Dich");
-            int lc;
+            Console.Write("Input the first numbers : ");
+            num1 = System.Convert.ToInt32(Console.ReadLine());
 
-            while (true)
-            {
-                Console.Write("Nhap Lua Chon : ");
-                lc = System.Int32.Parse(Console.ReadLine());
-                switch (lc)
-                {
-                    case 1:
-                        {
-                            Console.WriteLine("So Du : " + x.SoDu());
-
-                        }; break;
-
-                    case 2:
-                        {
-                            x.NapTien();
-                        }; break;
-
-                    case 3:
-                        {
-                            x.RutTien();
-                        }; break;
-                    case 4:
-                        {
-                            x.GiaoDich();
-                        }; break;
-                }
-            }
+            Console.Write("\nnput the second numbers : ");
+            num2 = System.Convert.ToInt32(Console.ReadLine());
         }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Value Error : " + e);
+        }
+
     }
 
+    public static void HandleValue(ref int num1, ref int num2)
+    {
 
+        try
+        {
+            Console.WriteLine("\n {0} divided by {1} is {2} ", num1, num2, System.Convert.ToSingle(num1) / System.Convert.ToSingle(num2));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Value Error : " + e);
+        }
+
+
+    }
+    public static void Main(string[] args)
+    {
+        int num1 = 0;
+        int num2 = 0;
+        InputValue(ref num1, ref num2);
+        HandleValue(ref num1, ref num2);
+    }
 }
