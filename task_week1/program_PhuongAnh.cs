3cad9d96815eab5fe9a38c77ce0f8f088693d0f0
TamKem <tamkem@gmail.com>
2023-04-07 23:20:24 +0700

    BaiTapTuan3

--- task_week1/program_PhuongAnh.cs
+++ task_week1/program_PhuongAnh.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace task_week1
+namespace bai16_toanTu3Ngoi
 {
-    internal class program_PhuongAnh
+    internal class Program
     {
+        static void Main(string[] args)
+        {
+            int tuoi;
+            tuoi = Convert.ToInt32(Console.ReadLine());
+            string kq = default;
+            kq = (tuoi < 18) ? "Tre Con" : "Nguoi Lon";
+            Console.WriteLine(kq);
+
+            Console.Write("Nhap thoi Tiet Hom Nay : ");
+            string thoitiet = Console.ReadLine();
+
+            string result = (thoitiet == "mua") ? "Troi Mua" : "Troi Nang";
+            Console.WriteLine(result);
+        }
     }
 }
