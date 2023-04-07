4bb39cf00cf5ac5195fd3a6c0f8e5b8338e8e34e
TamKem <tamkem@gmail.com>
2023-04-08 00:00:00 +0700

    update file week 3

--- task_week1/DucTuan/BaiTapTapTuan4_Expression.cs
+++ task_week1/DucTuan/BaiTapTapTuan4_Expression.cs
@@ -4,9 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace task_week1.DucTuan
+
+class Student
 {
-    internal class BaiTapTapTuan4_Expression
+    private string MSV;
+    private string HoTen;
+    private string Lop;
+
+    public Student(string mSV, string hoTen, string lop)
     {
+        this.MaSV = mSV;
+        this.HT = hoTen;
+        this.LopHoc = lop;
+    }
+
+    public string MaSV
+    {
+        get => MSV;
+        set => MSV = value;
+    }
+
+    public string HT
+    {
+        get => HoTen;
+        set => HoTen = value;
+    }
+
+    public string LopHoc
+    {
+        get => Lop;
+        set => Lop = value;
+    }
+
+    public void InTT()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Ma Sinh Vien : " + MSV);
+        Console.WriteLine("Ho Ten : " + HoTen);
+        Console.WriteLine("Lop : " + Lop);
+        Console.ForegroundColor = ConsoleColor.Black;
     }
 }
+
